cb0ca0203f3719df91ac516e30877e36c9eed872
WANG (William) Lipeng <wang.lp@outlook.com>
2018-09-20 12:28:09 +0800

    1. polish file blackhole test

--- src/test/uvrpc_client_file_blackhole.c
+++ src/test/uvrpc_client_file_blackhole.c
@@ -5,8 +5,21 @@
 #include <unistd.h>
 #include <pthread.h>
 #include <stdlib.h>
+#include <time.h>
+#include <sys/time.h>
 
-#define FILE_SIZE (1L * 1024 * 1024 * 1024 + 1)
+#define FILE_SIZE (65536)
+#define ITER_NUM (1000)
+
+int64_t get_wall_time() {
+    struct timeval time;
+    if (gettimeofday(&time, NULL)) {
+        //  Handle error
+        return 0;
+    }
+    return time.tv_sec * 1000000 + time.tv_usec;
+}
+
 
 int main(int argc, char **argv) {
     if (argc < 3) {
@@ -17,10 +30,22 @@ int main(int argc, char **argv) {
 
     char *buf = malloc(sizeof(char) * FILE_SIZE);
 
-    for (int i = 0; i < 100; i++) {
+    int64_t start_time = get_wall_time();
+    for (int i = 0; i < ITER_NUM; i++) {
+        int64_t chunk_start_time = get_wall_time();
         int ret = uvrpc_send(uvrpcc, buf, FILE_SIZE, 1);
-        printf("ret: %d\n", ret);
+        int64_t chunk_end_time = get_wall_time();
+        double chunk_time = (chunk_end_time - chunk_start_time) / 1000.0;
+        printf("time: %lfms, send size %ld, speed: %.3lfGB/s, ret: %d\n", chunk_time, FILE_SIZE,
+               (((double) FILE_SIZE) / (1024 * 1024 * 1024)) / (chunk_time/1000.0), ret);
     }
+    int64_t end_time = get_wall_time();
+
+    double total_time_in_ms = (end_time - start_time) / 1000.0;
+
+    printf("time: %.3lfms, send size: %ld, speed: %.3lfGB/s\n", total_time_in_ms, ITER_NUM * FILE_SIZE,
+           ((double) ITER_NUM * FILE_SIZE / (1024 * 1024 * 1024)) / (total_time_in_ms / 1000.0));
+
     //sleep(3);
     return 0;
 }
